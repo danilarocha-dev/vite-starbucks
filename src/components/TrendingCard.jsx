8dfdbaecc95f58c87bebb01dabc3215f14a436a1
Danila Rocha <danila.rocha.gloria@gmail.com>
2025-11-24 15:34:03 -0300

    page 'em alta' concluida

--- src/components/TrendingCard.jsx
+++ src/components/TrendingCard.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
 
-// Aceita props para tornar o card reutilizável para os 3 sabores
 const TrendingCard = ({imgSrc, title, flavor }) => {
   return (
-    // Equivalente a .trending-card { position: relative; }
     <section className='relative'>
       
       {/* Container da Imagem e Nota */}
       <div className="relative">
-        {/* Imagem: width clamp replicado com valor arbitrário */}
+      
         <img 
           src={imgSrc} 
           alt={`${title} ${flavor}`} 
-          className="w-[clamp(300px,5vw,600px)] rounded-lg" 
+          className="w-[clamp(300px,5vw,600px)] rounded-lg block" 
         />
 
         {/* Equivalente a .trending-rate */}
-        <div className="bg-neutral-0 absolute bottom-[70px] right-[30px] py-[5px] px-[10px] rounded-[5px]" >
-          <p className="flex items-center gap-1 font-semibold text-neutral-8">
+        <div className="
+        bg-neutral-0 absolute bottom-[10px] right-[30px] py-[5px] px-[10px] rounded-[5px]" >
+          <p className="flex items-center gap-1 font-semibold text-neutral-8 m-0">
             5 <i className="fa-solid fa-star text-secundary-1 "></i>
           </p>
         </div>
